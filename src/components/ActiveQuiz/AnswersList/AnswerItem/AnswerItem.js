33ac7ab5c7b956da0140a22b28044a47b584b5af
aekostyuk <a.e.kostyuk@gmail.com>
2021-04-02 19:23:33 +0300

    v 0.9

---
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -0,0 +1,18 @@
+import classes from './AnswerItem.module.css'
+
+const AnswersItem = props => {
+    const cls = [classes.AnswerItem]
+    if(props.state) {
+        cls.push(classes[props.state])
+    }
+    return (
+        <li
+            className={cls.join(' ')}
+            onClick={() => props.onAnswerClick(props.answer.id)}
+        >
+            {props.answer.text}
+        </li>
+    )
+}
+
+export default AnswersItem
